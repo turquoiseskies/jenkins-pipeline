ec174ca7b628a89435f58ea6665e6899e1f064ee
turquoiseskies <35639772+turquoiseskies@users.noreply.github.com>
2018-01-24 19:20:05 +0000

    Create pipeline.groovy

---
+++ vars/pipeline.groovy
@@ -0,0 +1,17 @@
+def call(Map pipelineParams) {
+      pipeline {
+          agent any
+          stages {
+              stage('build') {
+                  steps {
+                      sh 'mvn clean package'
+                  }
+              }
+               stage('code-quality-check') {
+                   steps {
+                       sh 'mvn sonar:sonar -Dsonar.host.url=http:172.17.0.3:9000 -Dsonar.login=c6eea290ca2ef147929dc78f402182570438a39f'
+                   }
+               }
+          }
+      }
+ }
