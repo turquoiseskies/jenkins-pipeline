450eda98ce766d198a363d4d94470c4bb5acc662
turquoiseskies <35639772+turquoiseskies@users.noreply.github.com>
2018-01-27 12:15:57 +0000

    fails if quality check fails

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -8,10 +8,21 @@ def call() {
                   }
               }
                stage('code-quality-check') {
-                   steps {
+                   steps {                         
+                     withSonarQubeEnv('My SonarQube Server') {
                        sh 'mvn sonar:sonar -Dsonar.host.url=http:172.17.0.2:9000 -Dsonar.login=c6eea290ca2ef147929dc78f402182570438a39f'
+                     } 
                    }
                }
+                
+              stage("Quality Gate"){
+                timeout(time: 2, unit: 'MINUTES') {
+                def qg = waitForQualityGate()
+                if (qg.status != 'OK') {
+                  error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                }
+          }
+      }
           }
       }
  }
