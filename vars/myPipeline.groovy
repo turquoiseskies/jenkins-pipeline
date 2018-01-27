1c2ca4b35e5f56c21e651afcfd42533ac1c79593
turquoiseskies <35639772+turquoiseskies@users.noreply.github.com>
2018-01-27 12:30:50 +0000

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -18,11 +18,14 @@ def call() {
               stage("Quality Gate"){
                     steps {   
                       timeout(time: 2, unit: 'MINUTES') {
-                      def qg = waitForQualityGate()
-                            if (qg.status != 'OK') {
-                              error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                      
+                            script {
+                                  def qg = waitForQualityGate()
+                                  if (qg.status != 'OK') {
+                                     error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                                  }
                             }
-                      }
+                       }
                     }
              }
           }
