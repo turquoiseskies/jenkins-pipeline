668ee52bdd193d168a96fd983f61be7381f1cadd
gavin samuel <gavin.s999@googlemail.com>
2018-02-03 15:34:15 +0000

    added bake image step

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -30,6 +30,19 @@ def call() {
 
                     }
              }
+
+              stage("bake image") {
+
+                  steps {
+                      docker.withRegistry('https://registry:5000') {
+
+                          def customImage = docker.build("my-image:${env.BUILD_ID}")
+
+                          /* Push the container to the custom Registry */
+                          customImage.push()
+                      }
+                  }
+              }
           }
       }
  }
